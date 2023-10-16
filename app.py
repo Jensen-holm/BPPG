f3e495fde54e92682901a69cec225a0b5e72ce82
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-16 19:56:24 -0400

    added upload file button and setup minsize

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 import tkinter as tk
 import customtkinter as ctk
+import PIL
 
 ctk.set_appearance_mode("system")
 
 root = ctk.CTk()
 root.geometry("800x600")
 root.title("Backprop Playground")
+root.minsize(600, 400)
 
-button = ctk.CTkButton(
-    master=root,
-    text="train",
+
+upload = PIL.Image.open("assets/upload.png")
+upload_img = ctk.CTkImage(
+    light_image=upload,
+    dark_image=upload,
 )
 
-button.place(
+upload_csv_button = ctk.CTkButton(
+    text="upload csv",
+    image=upload_img,
+    master=root,
+)
+
+upload_csv_button.place(
     relx=0.5,
     rely=0.5,
     anchor=tk.CENTER,
