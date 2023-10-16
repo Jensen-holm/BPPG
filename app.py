8c514fb09ebc452063350b38d359de8d6f4db16c
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-16 11:56:38 -0400

    basic hello world customtkinter gui

--- app.py
+++ app.py
@@ -1,48 +1,16 @@
-from flask import Flask, request, jsonify, Response
-from flask_cors import CORS
-from nn.nn import NN
-from nn import train as train_nn
-from nn import activation
-import pandas as pd
-import io
-
-app = Flask(__name__)
-
-CORS(app, origins="*")
+import tkinter as tk
+from customtkinter import *
 
 
-@app.route("/neural-network", methods=["POST"])
-def neural_net():
-    args = request.json
+class App(CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    try:
-        net = NN.from_dict(args)
-    except Exception as e:
-        return Response(
-            response=f"issue with request args: {e}",
-            status=400,
-        )
-
-    try:
-        df = pd.read_csv(io.StringIO(net.data))
-        net.set_df(df=df)
-    except Exception as e:
-        return Response(
-            response=f"error reading csv data: {e}",
-            status=400,
-        )
-
-    try:
-        activation.get_activation(nn=net)
-    except Exception:
-        return Response(
-            response="invalid activation function",
-            status=400,
-        )
-
-    result = train_nn.train(nn=net)
-    return jsonify(result)
+        self.title("Backprop Playground")
+        self.geometry("800x600")
 
 
 if __name__ == "__main__":
-    app.run()
+    set_appearance_mode("system")
+    app = App()
+    app.mainloop()
